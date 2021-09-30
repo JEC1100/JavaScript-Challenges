3b0fb17c2e6c5b56e5641a3b79a25ac4b38cefc5
jamescondon <jecondon66@icloud.com>
2021-09-30 23:41:48 +0100

    Passes third example dial code and number

--- spec/create_phone_number_spec.rb
+++ spec/create_phone_number_spec.rb
@@ -7,4 +7,7 @@ describe '#create_phone_number' do
   it "outputs phone number string for second test example" do
     expect(create_phone_number([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])).to eq "(111) 111-1111"
   end
+  it "outputs phone number string for third test example" do
+    expect(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])).to eq "(123) 456-7890"
+  end 
 end
